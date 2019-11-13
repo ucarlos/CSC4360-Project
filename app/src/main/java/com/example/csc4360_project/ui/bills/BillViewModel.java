efdaaa97fbc5d3d7ea328d644f44d29e71c730c0
Ulysses Carlos <ulycarlos@gmail.com>
2019-11-13 00:23:59 -0500

    Example Commit -- Please ignore.

---
+++ app/src/main/java/com/example/csc4360_project/ui/bills/BillViewModel.java
@@ -0,0 +1,8 @@
+package com.example.csc4360_project.ui.bills;
+
+import androidx.lifecycle.ViewModel;
+
+public class BillViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+    // IGNORE THIS
+}
