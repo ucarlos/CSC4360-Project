fa5d645a9ebaf4d383628476d486f27da14aaaa3
Ulysses Carlos <ulycarlos@gmail.com>
2019-12-01 21:50:19 -0500

    Secondary Commit

---
+++ app/src/main/java/com/example/csc4360_project/Medicine_Form.java
@@ -0,0 +1,14 @@
+package com.example.csc4360_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Medicine_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
